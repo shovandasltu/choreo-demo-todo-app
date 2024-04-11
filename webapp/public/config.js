2672809e897dde854bf8a9ed17d1c0c37b8e5754
Shovan <shovandas@hptmail.com>
2024-04-11 14:55:43 +0000

    Configuration Changed

--- webapp/public/config.js
+++ webapp/public/config.js
@@ -1,10 +1,10 @@
 window.config = {
   todoApiUrl:
-    "https://ec149f4e-f145-4064-a8ab-e1fc8f0c563f-dev.e1-us-east-azure.choreoapis.dev/rgyo/todo-svc/graphql-todo-aa9/1.0.0/",
+    "https://05c9efee-c157-46ec-a8d8-392f3939e179-dev.e1-us-east-azure.choreoapis.dev/choreo-todo-app/todo-service/graphql-todo-aa9/v1",
   auth: {
-    signInRedirectURL: "http://localhost:5173",
-    signOutRedirectURL: "http://localhost:5173",
-    clientID: "DXmO3q4E4IAJx9dZE5yyNATRjO4a",
-    baseUrl: "https://api.asgardeo.io/t/mirajabeysekara",
+    signInRedirectURL: "https://b6c6fd07-883b-4de8-8b62-e74ad179f214.e1-us-east-azure.choreoapps.dev",
+    signOutRedirectURL: "https://b6c6fd07-883b-4de8-8b62-e74ad179f214.e1-us-east-azure.choreoapps.dev",
+    clientID: "d1ckgvB7V3JHAC4GyphlgStCODoa",
+    baseUrl: "https://api.asgardeo.io/t/skyliveonline",
   },
 };
